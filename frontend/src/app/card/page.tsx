a3dfa119e44625d34d97db18fd815c50af2b12ad
Bartosz Nowak <bartoszn337@gmail.com>
2023-11-19 01:05:06 +0100

    no gif

--- frontend/src/app/card/page.tsx
+++ frontend/src/app/card/page.tsx
@@ -14,7 +14,7 @@ const drinks = [
 export default function Page() {
   return (
     <main className="relative h-screen w-auto flex items-center justify-center overflow-x-hidden overflow-y-auto">
-      <div className="min-h-screen w-screen overflow-x-hidden mx-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black/50">
+      {/* <div className="min-h-screen w-screen overflow-x-hidden mx-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black/50">
         <div className="bg-black/75 absolute w-screen h-screen top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
         <video
           autoPlay
@@ -25,7 +25,7 @@ export default function Page() {
           <source src="bg-video.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-      </div>
+      </div> */}
 
       <Info />
     </main>
